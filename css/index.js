6be76af21f927beb8d4ac70af8930cf145269732
Mkalabai John <nakutakaa@gmail.com>
2025-03-28 12:46:15 +0300

    carInventoryIndex.js

---
+++ css/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const carsContainer = document.getElementById("cars-container");
+  const addCarForm = document.getElementById("add-car-form");
+  const versionInputs = document.querySelector(".version-inputs");
+  const addVersionBtn = document.getElementById("add-version-btn");
+  let currentCarId = null;
+
+    
+    
